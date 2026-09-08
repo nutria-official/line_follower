86821470d7a21a957e267abf0ecedcb79b650762
nutria-official <nutria.official@proton.me>
2026-09-08 10:32:45 +0200

    work

--- motor_test.ino
+++ motor_test.ino
@@ -1,9 +1,23 @@
-int E1 = 6;
-int M1 = 7;
-int E2 = 5;
-int M2 = 4;
 
-int speed = 0;
+// Motor pins:
+#define E1  6;
+#define M1  7;
+#define E2  5;
+#define M2  4;
+
+// Sensor pins:
+
+#define r_sensor 11
+#define l_sensor 10
+#define c_sensor 9
+
+#define ar_sensor "A0"
+#define al_sensor "A1"
+#define ac_sensor "A2"
+
+uint8_t RS = 0; // Right speed.
+uint8_t LS = 0; // Left speed.
+
 void setup()
 {
     pinMode(M1, OUTPUT);
@@ -20,4 +34,8 @@ void loop() {
   if (speed >= 255) {
     speed = 0;
   }
+}
+
+void drive() {
+
 }
